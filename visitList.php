8366af7f143225f3fc025e14781bffa5968aaa2b
KevinsProgramming <B00157434@mytudublin.ie>
2024-10-27 14:58:19 +0000

    NC4

--- visitList.php
+++ visitList.php
@@ -2,26 +2,39 @@
 
 include("dbcon.php");
 
-$sql = "SELECT visit.visit_date, visit.visit_time, patient.firstname AS PatientFirstName, patient.surname AS PatientSurname doctor.Firstanme, doctor.Surname, doctor.specialist FROM visit
-INNER JOIN patient ON
-visit.patient_id = patient.id
-INNER JOIN doctor ON
-visit.doctor_id = doctor.id";
+$sql = "SELECT 
+            visit.visit_date AS VisitDate, 
+            visit.visit_time AS VisitTime, 
+            patient.firstname AS PatientFirstname, 
+            patient.surname AS PatientSurname, 
+            doctor.firstname AS DoctorFirstname, 
+            doctor.surname AS DoctorSurname, 
+            doctor.specialism AS Specialism 
+        FROM visit
+        INNER JOIN patient ON visit.patient_id = patient.id
+        INNER JOIN doctor ON visit.doctor_id = doctor.id";
 
+$result = mysqli_query($conn, $sql);
 
-$result = mysqli_query($conn,$sql);
-while($row=mysqli_fetch_assoc($result)) {
-    $vdate = $row['visit_date'];
-    $vtime = $row['visit_time'];
+while ($row = mysqli_fetch_assoc($result)) {
+    $vdate = $row['VisitDate'];
+    $vtime = $row['VisitTime'];
     $piname = $row['PatientFirstname'];
     $pisurn = $row['PatientSurname'];
-    $dfname = $row['Firstname'];
-    $dsurn = $row['Surname'];
+    $dfname = $row['DoctorFirstname'];
+    $dsurn = $row['DoctorSurname'];
     $dspecial = $row['Specialism'];
-    
 
-
-    echo "<TR><TD>$vdate</TD><TD>$vtime</TD><TD>$piname</TD><TR><TD>$pisurn</TD><TR><TD>$dfname</TD><TR><TD>$dsurn</TD><TR><TD>$dspecial</TD>";    
+    echo "<tr>
+            <td>$vdate</td>
+            <td>$vtime</td>
+            <td>$piname</td>
+            <td>$pisurn</td>
+            <td>$dfname</td>
+            <td>$dsurn</td>
+            <td>$dspecial</td>
+          </tr>";
 }
+
 mysqli_close($conn);
 ?>
