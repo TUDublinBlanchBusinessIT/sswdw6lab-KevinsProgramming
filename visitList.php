6848f6cc77e0c2777aa156f909be75715acf00d4
KevinsProgramming <B00157434@mytudublin.ie>
2024-10-27 14:41:16 +0000

    changes

--- visitList.php
+++ visitList.php
@@ -2,14 +2,26 @@
 
 include("dbcon.php");
 
-$sql = "select * from visit";
+$sql = "SELECT visit.visit_date, visit.visit_time, patient.firstname AS PatientFirstName, patient.surname AS PatientSurname doctor.Firstanme, doctor.Surname, doctor.specialist FROM visit
+INNER JOIN patient ON
+visit.patient_id = patient.id
+INNER JOIN doctor ON
+visit.doctor_id = doctor.id";
+
+
 $result = mysqli_query($conn,$sql);
 while($row=mysqli_fetch_assoc($result)) {
     $vdate = $row['visit_date'];
     $vtime = $row['visit_time'];
-    $pid = $row['patient_id'];
+    $piname = $row['PatientFirstname'];
+    $pisurn = $row['PatientSurname'];
+    $dfname = $row['Firstname'];
+    $dsurn = $row['Surname'];
+    $dspecial = $row['Specialism'];
     
-    echo "<TR><TD>$vdate</TD><TD>$vtime</TD><TD>$pid</TD>";    
+
+
+    echo "<TR><TD>$vdate</TD><TD>$vtime</TD><TD>$piname</TD><TR><TD>$pisurn</TD><TR><TD>$dfname</TD><TR><TD>$dsurn</TD><TR><TD>$dspecial</TD>";    
 }
 mysqli_close($conn);
 ?>
